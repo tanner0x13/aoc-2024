05362b24e27f5af64b516cd2d2f3269412708029
Tanner Zigrang <tanner.zigrang@gmail.com>
2024-12-01 19:06:52 -0800

    complete day 1, star 2

---
+++ src/day1_2.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+    // validate args
+    if (argc != 2) {
+        std::cout << "USAGE: ./a.out path/to/input.txt" << std::endl;
+        return 1;
+    }
+
+    // read input file
+    std::string filename = argv[1];
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cout << "ERROR: could not open " << filename << std::endl;
+        return 1;
+    }
+
+    // parse input file
+    std::vector<int> left_list;
+    std::vector<int> right_list;
+    std::string line;
+    while (std::getline(file, line)) {
+        int l, r;
+        std::stringstream ss(line);
+        ss >> l;
+        ss >> r;
+        left_list.push_back(l);
+        right_list.push_back(r);
+    }
+    file.close();
+
+    // construct map from left list values
+    std::map<int, int> m;
+    for (const int& v : left_list) {
+        m[v] = 0;
+    }
+
+    // count occurrences in right list
+    for (const int& v : right_list) {
+        m[v] += 1;
+    }
+
+    // calculate answer by iterating left list
+    int ans = 0;
+    for (const int& v : left_list) {
+        ans += (v * m[v]);
+    }
+
+    // print result and exit
+    std::cout << "ANSWER: " << ans << std::endl;
+    return 0;
+}
